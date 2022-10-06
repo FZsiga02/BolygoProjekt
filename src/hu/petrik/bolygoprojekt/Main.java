ff986f3232e3f4a841746c27e5a529083175adf4
FZsiga02 <fzsigmond02@gmail.com>
2022-10-06 10:35:22 +0200

    Bolygó feladatsor

--- src/hu/petrik/bolygoprojekt/Main.java
+++ src/hu/petrik/bolygoprojekt/Main.java
@@ -1,7 +1,17 @@
 package hu.petrik.bolygoprojekt;
 
+import hu.petrik.bolygoprojekt.bolygo.Bolygo;
+
 public class Main {
     public static void main(String[] args) {
+        for (Bolygo b: Bolygo.values()){
+            System.out.printf("%15s %35f %20f\n", b, b.getTomeg(), b.getSugar());
+        }
+        int sulyAFoldon = 100;
+        int tomeg = (int) (sulyAFoldon / Bolygo.FOLD.getGravitacio());
 
+        for (Bolygo b: Bolygo.values()){
+            System.out.printf("100kg a %15s - %f\n", b, b.getSuly(tomeg));
+        }
     }
 }
